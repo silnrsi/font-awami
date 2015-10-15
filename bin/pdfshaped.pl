a0f51e46a370fdb84b7bd9a4f5b2b3b91ce83d12
Martin Hosken <martin_hosken@sil.org>
2015-10-15 15:58:23 +0000

    Add pdf smith target to build pdfs using cmptxtrender and pdfshaped.pl

---
+++ bin/pdfshaped.pl
@@ -0,0 +1,77 @@
+
+use Font::TTF::Font;
+use Text::PDF::File;
+use Text::PDF::Page;
+use Text::PDF::TTFont0;
+use Text::PDF::SFont;
+use Text::PDF::Utils;
+use JSON;
+use Getopt::Long;
+
+%opts = (size => 12, linespacing => 1.2, margins => 0.8, pagesize => 'a4', labelwidth => 0.5, labelsize => 9.5);
+GetOptions(\%opts, 'font|f=s', 'output|o=s', 'size|s=i', 'linespacing|l=f', 'margins|m=f',
+            'pagesize|p', 'labelwidth|w=f', 'labelsize|ls=f');
+
+my %sizes = (
+    'a4' => [595, 842],
+    'ltr' => [612, 792]
+);
+
+my $pdf = Text::PDF::File->new;
+$pdf->create_file($opts{'output'}) || die "Can't create pdf file $opts{'output'}";
+$pdf->{' version'} = 3;
+my $root = Text::PDF::Pages->new($pdf);
+$root->proc_set('PDF', 'Text');
+my ($maxx, $maxy) = @{$sizes{$opts{'pagesize'}}};
+$root->bbox(0, 0, $maxx, $maxy);
+my $font = Font::TTF::Font->open($opts{'font'}) || die "Can't open font file $opts{'font'} for reading";
+my $tfont = Text::PDF::TTFont0->new($pdf, $font, "Ta0");
+$root->add_font($tfont, $pdf);
+$pdf_helv = Text::PDF::SFont->new($pdf, 'Helvetica', "FR");
+$root->add_font($pdf_helv, $pdf);
+my $t;
+while(<>) { $t .= $_; }
+my $json = decode_json($t);
+
+my $page;
+my ($curry, $page);
+my ($upem) = $font->{'head'}{'unitsPerEm'};
+my ($post) = $font->{'post'}->read;
+my (@lines) = sort { $a <=> $b || $a cmp $b } keys %{$json};
+foreach my $k (@lines)
+{
+    my ($v) = $json->{$k};
+    unless (defined $page)
+    {
+        $page = Text::PDF::Page->new($pdf, $root);
+        $curry = $maxy - $opts{'margins'} * 72 - $opts{'linespacing'} * $opts{'size'};
+    }
+    my ($currx) = ($opts{'margins'} + $opts{'labelwidth'}) * 72;
+    my ($lwidth) = $pdf_helv->width($k) * 9.5 * 0.8;
+    $page->add(sprintf("BT 1 0 0 1 %.2f %.2f Tm /FR %f Tf 80 Tz (%s) Tj ET\n", $currx - $lwidth - 3, $curry, $opts{'labelsize'}, $k));
+    foreach my $glyph (@{$v->[0]})
+    {
+        next if ($glyph->[0] eq '_adv_');
+        my ($gid) = $post->{'STRINGS'}{$glyph->[0]};
+        my ($x) = $glyph->[1] * $opts{'size'} / $upem + $currx;
+        my ($y) = $glyph->[2] * $opts{'size'} / $upem + $curry;
+        if ($x < $maxx)
+        {
+            $page->add(sprintf("BT 1 0 0 1 %.2f %.2f Tm /Ta0 %d Tf 100 Tz <%04X> Tj ET\n", $x, $y, $opts{'size'}, $gid));
+        }
+    }
+    $curry -= $opts{'linespacing'} * $opts{'size'};
+    if ($curry < $opts{'margins'} * 72)
+    {
+        $page->{' curstrm'}{'Filter'} = PDFArray(PDFName('FlateDecode'));
+        $page->ship_out($pdf);
+        $page = undef;
+    }
+}
+if (defined $page)
+{
+    $page->{' curstrm'}{'Filter'} = PDFArray(PDFName('FlateDecode'));
+    $page->ship_out($pdf);
+}
+$pdf->close_file;
+
--- wscript
+++ wscript
@@ -4,6 +4,12 @@ COPYRIGHT='Copyright 2015, SIL International. All rights reserved.'
 APPNAME='Awami'
 VERSION='0.1'
 
+tests = fonttest(extras = {
+            'pdf' : tests({
+                'pdfs' : cmd("${CMPTXTRENDER} -t ${SRC[1]} -e ${shaper} --outputtype=json -r ${SRC[0]} | ${PDFSHAPED} -s 16 -l 2.0 -o ${TGT} -f ${SRC[0]}")
+            }, ext=".pdf")})
+
+
 font(target = 'Awami_test.ttf',
      source = create('temp/Awami_full.sfd',
                 cmd("${FFCOPYGLYPHS} -i ../DoulosSIL-R.ttf -r 21..7E -f ${SRC} ${TGT}", ['Awami Nastaliq Regular.ttf'])),
@@ -12,9 +18,11 @@ font(target = 'Awami_test.ttf',
      ap = "Awami Nastaliq Regular_tmp.xml",
      license = ofl(file='OFL.txt'),
      copyright = COPYRIGHT,
-     extra_srcs = ['bin/awami_makegdl', 'bin/FFcopyGlyphs.py', 'bin/perllib/Font/TTF/Scripts/GDL.pm', 'DoulosSIL-R.ttf']
+     extra_srcs = ['bin/awami_makegdl', 'bin/FFcopyGlyphs.py', 'bin/perllib/Font/TTF/Scripts/GDL.pm', 'DoulosSIL-R.ttf'],
+     tests = tests
     )
 
 def configure(ctx) :
     ctx.env['MAKE_GDL'] = 'perl -I ../bin/perllib ../bin/awami_makegdl'
     ctx.env['FFCOPYGLYPHS'] = '../bin/FFcopyGlyphs.py'
+    ctx.env['PDFSHAPED'] = 'perl ../bin/pdfshaped.pl'
