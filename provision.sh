a46d9dc7b3360c24d4b9ab590ac7c58c8021b19c
Nicolas Spalinger <nicolas.spalinger@sil.org>
2016-05-23 21:40:05 +0200

    Dropped the grcompiler from-source recipe, we now use the .deb package from the PPA. Dropped the dist-upgrade stanza.

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # A provisioning script to get the latest smith & friends
-# and to compile grcompiler, pysilfont and python-palaso from source 
+# and to compile graphite from source 
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -17,8 +17,6 @@ sudo add-apt-repository -s -y ppa:silnrsi/smith
 sudo add-apt-repository -s -y ppa:fontforge/fontforge 
 
 apt-get update -y  
-
-apt-get dist-upgrade -y 
 
 sudo apt-get install -y smith fontforge 
 
@@ -49,29 +47,6 @@ cmake ..
 make
 sudo make install 
 
-
-
-echo "Installing grcompiler from source"
-echo " "
-sudo apt-get install docbook2x docbook-xml docbook-utils libicu-dev autoconf2.59 -y 
-
-cd ~/builds
-
-svn checkout https://scripts.sil.org/svn-public/graphite/grcompiler/trunk grcompiler
-
-cd grcompiler 
-
-autoreconf -i
-
-mkdir build
-
-cd build 
-
-../configure
-
-make
-
-sudo make install 
 
 echo " "
 echo "Done!" 
