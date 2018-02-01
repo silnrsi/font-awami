5cce2c6ab3eeb8abe068299fb723fe2e28381d00
Sharon Correll <sharon_correll@sil.org>
2018-02-01 16:52:24 -0600

    Changes to work with GlyphsApp-generated files.

--- finish_update.bat
+++ finish_update.bat
@@ -1,3 +1,4 @@
-copy "source\AwamiNastaliqRegular.ttf" results\Awami_gr.ttf
+copy "results\AwamiNastaliq-Dev.ttf" results\Awami_gr.ttf
+copy "results\AwamiNastaliq-Dev.gdx" results\Awami_gr.gdx"
 
 run_makegdl_SC.bat
